14a80ed6bee7e70575e4c7619703186c5ad97cf1
SavenokNikolay <savenok.kolya.nikolay@gmail.com>
2023-06-20 21:52:48 +0300

    change

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 'use strict';
+const number = +prompt('Введіть число');
+const power = +prompt('Введіть сутпінь');
+const result = pow(Number(number), Number(power));
+
 function pow(num, degree) {
+
+   if (num < 0 || power < 0 || isNaN(num) || isNaN(power) || !Number.isInteger(num) || !Number.isInteger(power)) {
+      return 'Please enter only number!';
+   }
    if (degree === 0) {
       return 1;
    }
    return num * pow(num, degree - 1)
 }
 
-const number = +prompt('Введіть число');
-const power = +prompt('Введіть сутпінь');
-const result = pow(Number(number), Number(power));
-
-if (!number || !power || !Number.isInteger(number)) {
-   console.log('Number no valid')
-} else {
-   console.log(result);
-}
+console.log(result)
 
 
 
